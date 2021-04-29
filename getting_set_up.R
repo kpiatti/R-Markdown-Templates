2c329c562f41f0cc6c582075db6a1a1876b9d356
Katherine Piatti <kpiatti@gmail.com>
2021-04-29 14:01:54 -0500

    setup rproj and initial script

---
+++ getting_set_up.R
@@ -0,0 +1,16 @@
+##########################################################
+  
+# R Package for Custom R Markdown Templates
+  
+########################################################## 
+  
+library(devtools)
+library(roxygen2)
+
+
+# before i can move forward, i need to:
+# read about and install Rtools (get link from mike's post on slack)
+# retry the roxygen2 library command, if necessary figure out database is corrupt error
+
+
+# here is the bookdown tutorial i'm using: https://rstudio4edu.github.io/rstudio4edu-book/data-pkg.html
